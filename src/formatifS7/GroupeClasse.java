e9b09793309de7f930d3f3dfe3b9cce25c10135f
TomGoidin <2412355@cegeplimoilou.ca>
2026-03-12 16:51:49 -0400

    Changement de la variable static final MAXIMUM_ETU

--- src/formatifS7/GroupeClasse.java
+++ src/formatifS7/GroupeClasse.java
@@ -7,9 +7,9 @@ package formatifS7;
  * @since 2022-11-16
  */
 public class GroupeClasse {
-    public static final int MAX_ETUDIANTS = 26;
+    public static final int MAXIMUM_ETU = 26;
 
-    private Etudiant[] etudiants = new Etudiant[MAX_ETUDIANTS];
+    private Etudiant[] etudiants = new Etudiant[MAXIMUM_ETU];
 
     private String nom;
 
@@ -26,7 +26,7 @@ public class GroupeClasse {
      * @param etudiant
      */
     public void addEtudiant(Etudiant etudiant){
-        if(nombreEtudiants < MAX_ETUDIANTS) {
+        if(nombreEtudiants < MAXIMUM_ETU) {
             //on peut insérer
             etudiants[nombreEtudiants] = etudiant;
             nombreEtudiants++;
